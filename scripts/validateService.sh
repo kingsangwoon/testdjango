97c333aab083b2438ba37b6bfa9875e22d0e4dfc
KINGSANGWOON <sangwoon0530@gmail.com>
2022-10-31 13:38:22 +0900

    Create validateService.sh

---
+++ scripts/validateService.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for i in $(seq 1 6)
+do
+    HTTP_CODE=$(curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://localhost:80/health)
+    if [ "${HTTP_CODE}" == "200" ]; then
+        echo "Succeeded pull healthcheck page."
+        exit 0
+    fi
+    echo "Attempt to curl endpoint returned HTTP Code ${HTTP_CODE}. Backing off and retrying."
+    sleep 10
+done
+echo "Server did not come up after expected time. Failing"
+exit 1
