7efe0a1371df2bb60f6f3ffa8598130e3de0f54d
KINGSANGWOON <sangwoon0530@gmail.com>
2022-10-31 15:32:45 +0900

    Update applicationStart.sh

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /webapp/
-nohup .venv/bin/python3 manage.py runserver > nohup_script.out 2>&1 & echo $! > app.pid
+nohup .venv/bin/python3 manage.py runserver 0.0.0.0:8000 > nohup_script.out 2>&1 & echo $! > app.pid
 
